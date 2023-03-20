26797f3cb3971ca5e26e817002a0ce98b6163a02
MBOGLEN-BION <pbion73@gmail.com>
2023-03-20 13:47:32 +0100

    traitement des conditions

---
+++ firstStep/Conditions.py
@@ -0,0 +1,23 @@
+taille = 1.6
+sexe = 'M'
+
+# if
+
+if taille > 2:
+    print("tu es un volleyeur")
+    
+# if...else
+
+if taille > 2:
+    print("tu es un volleyeur")
+else:
+    print("tu ne peux pas jouer au volley")
+
+# if...elif...else
+
+if taille > 2:
+    print("tu es un volleyeur")
+elif taille >= 1.5 and taille <= 2:
+    print("tu es un passeur")
+else:
+    print("tu ne peux pas jouer au volley")
